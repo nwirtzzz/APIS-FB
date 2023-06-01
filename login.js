09e785194f0963df1b1c8998bbf386ae7806c828
facurattagan10 <131196888+facurattagan10@users.noreply.github.com>
2023-06-01 11:50:04 -0300

    login falta

--- login.js
+++ login.js
@@ -1,112 +1,142 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
 
-// Configuración de la conexión a MongoDB
-mongoose.connect('URL_DE_CONEXIÓN_A_MONGODB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
-db.once('open', () => {
-  console.log('Conexión exitosa a MongoDB');
-});
-
-// Definición del esquema del usuario
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-const User = mongoose.model('User', userSchema);
-
-// Configuración de Express
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  session({
-    secret: 'mi_secreto',
-    resave: false,
-    saveUninitialized: true
-  })
-);
+const port = 3000;
 
-// Rutas
+// Configurar middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'mysecretkey',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Configurar la conexión a la base de datos (MongoDB)
+
+// Definir el modelo de usuario
+const User = require('./models/user');
+
+// Ruta para el formulario de inicio de sesión
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(__dirname + '/views/login/login.html');
 });
 
+// Ruta para procesar el inicio de sesión
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
-  // Buscar al usuario en la base de datos
-  const user = await User.findOne({ username });
+  try {
+    // Buscar al usuario en la base de datos
+    const user = await User.findOne({ username });
 
-  if (!user) {
-    // Usuario no encontrado
-    return res.render('index', { error: 'Usuario o contraseña incorrectos' });
+    // Verificar si el usuario existe y si las contraseñas coinciden
+    if (user && await bcrypt.compare(password, user.password)) {
+      // Iniciar sesión y redirigir al usuario a la página de inicio
+      req.session.userId = user._id;
+      res.json({ success: true });
+    } else {
+      // Las credenciales son inválidas
+      res.status(401).json({ success: false, message: 'Credenciales inválidas' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error del servidor' });
   }
-
-  // Verificar la contraseña
-  const passwordMatch = await bcrypt.compare(password, user.password);
-
-  if (!passwordMatch) {
-    // Contraseña incorrecta
-    return res.render('index', { error: 'Usuario o contraseña incorrectos' });
-  }
-
-  // Iniciar sesión
-  req.session.userId = user._id;
-  res.redirect('/dashboard');
 });
 
+// Ruta para el panel de control (requiere inicio de sesión)
 app.get('/dashboard', (req, res) => {
-  if (!req.session.userId) {
-    // El usuario no ha iniciado sesión
-    return res.redirect('/');
+  if (req.session.userId) {
+    // El usuario ha iniciado sesión, mostrar el panel de control
+    res.sendFile(__dirname + '/views/dashboard.html');
+  } else {
+    // El usuario no ha iniciado sesión, redirigir al formulario de inicio de sesión
+    res.redirect('/');
   }
-
-  // Renderizar el dashboard
-  res.render('dashboard');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor iniciado en http://localhost:3000');
+// Ruta para cerrar sesión
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
 });
 
-// NUEVO 2.0
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
 
-app.get('/register', (req, res) => {
-    res.render('register');
-  });
-  
-  app.post('/register', async (req, res) => {
+const mongoose = require('mongoose');
+
+// Conexión a la base de datos
+mongoose.connect('mongodb://localhost/Facur', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Verificar la conexión exitosa
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conexión exitosa a la base de datos MongoDB');
+});
+
+
+//REGISTRO ruta hacia el
+
+app.post('/register', async (req, res) => {
     const { username, password } = req.body;
   
+    try {
+      // Verificar si el usuario ya existe en la base de datos
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res.render('register', { error: 'El usuario ya existe' });
+      }
+  
+      // Crear un nuevo usuario
+      const newUser = new User({ username, password });
+      await newUser.save();
+  
+      // Redirigir al usuario a la página de inicio de sesión
+      res.redirect('/');
+    } catch (error) {
+      console.error(error);
+      res.render('register', { error: 'Error del servidor' });
+    }
+  });
+  
+
+// parte 2 del registro para guardar los datos del registro
+
+const User = require('./models/user'); // Asegúrate de importar el modelo de usuario
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
     // Verificar si el usuario ya existe en la base de datos
     const existingUser = await User.findOne({ username });
-  
     if (existingUser) {
-      // El usuario ya está registrado
-      return res.render('register', { error: 'El usuario ya está registrado' });
+      return res.render('register', { error: 'El usuario ya existe' });
     }
-  
-    // Crear un nuevo usuario en la base de datos
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword });
+
+    // Crear un nuevo usuario
+    const newUser = new User({ username, password });
     await newUser.save();
-  
-    // Iniciar sesión automáticamente después del registro
-    req.session.userId = newUser._id;
-  
-    res.redirect('/dashboard');
-  });
-  
-// PARTE 3
-app.get('/logout', (req, res) => {
-    req.session.destroy();
+
+    // Redirigir al usuario a la página de inicio de sesión
     res.redirect('/');
-  });
-  
+  } catch (error) {
+    console.error(error);
+    res.render('register', { error: 'Error del servidor' });
+  }
+});
+
+
+
+
