594b9bd57771c2ca182a2a0c3947721618a9ff8f
facurattagan10 <131196888+facurattagan10@users.noreply.github.com>
2023-06-01 11:01:31 -0300

    login

--- login.js
+++ login.js
@@ -75,3 +75,38 @@ app.get('/dashboard', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor iniciado en http://localhost:3000');
 });
+
+// NUEVO 2.0
+
+app.get('/register', (req, res) => {
+    res.render('register');
+  });
+  
+  app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+  
+    // Verificar si el usuario ya existe en la base de datos
+    const existingUser = await User.findOne({ username });
+  
+    if (existingUser) {
+      // El usuario ya está registrado
+      return res.render('register', { error: 'El usuario ya está registrado' });
+    }
+  
+    // Crear un nuevo usuario en la base de datos
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+  
+    // Iniciar sesión automáticamente después del registro
+    req.session.userId = newUser._id;
+  
+    res.redirect('/dashboard');
+  });
+  
+// PARTE 3
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+  });
+  
