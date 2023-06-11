f410350049ddf93fa224b6f2ad3e985bf7bd4238
nwirtzzz <112899436+nwirtzzz@users.noreply.github.com>
2023-06-11 17:53:52 -0300

    filtro
    filtro

---
+++ equipos.js
@@ -0,0 +1,43 @@
+const teamIds = [33, 34];
+
+for (let i = 0; i < teamIds.length; i++) {
+  const teamId = teamIds[i];
+  fetch(`https://v3.football.api-sports.io/teams?id=${teamId}`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "v3.football.api-sports.io",
+      "x-rapidapi-key": "94861d9837c67ac2b84dff859ac9c73c"
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    const team = data.response[0].team;
+    console.log(data)
+    const teamName = team.name;
+    const teamLogoUrl = team.logo;
+    const teamLogo = document.createElement('img');
+    teamLogo.src = teamLogoUrl;
+
+    const teamContainer = document.createElement('div');
+    teamContainer.classList.add('team-container');
+
+    const teamNameElement = document.createElement('h2');
+    teamNameElement.textContent = teamName;
+
+    teamContainer.appendChild(teamNameElement);
+    teamContainer.appendChild(teamLogo);
+    
+    // Add event listener to each team container
+    teamContainer.addEventListener('click', () => {
+      // Store the team data in local storage
+      localStorage.setItem('selectedTeam', JSON.stringify(team));
+      // Redirect to the separate page
+      window.location.href = 'team.html';
+    });
+
+    document.getElementById('teams').appendChild(teamContainer);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
--- jugadores.js
+++ jugadores.js
@@ -1,48 +1,4 @@
-const teamIds = [33, 34, 35, 36, 37];
-
-for (let i = 0; i < teamIds.length; i++) {
-  const teamId = teamIds[i];
-  fetch(`https://v3.football.api-sports.io/teams?id=${teamId}`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "v3.football.api-sports.io",
-      "x-rapidapi-key": "94861d9837c67ac2b84dff859ac9c73c"
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    const team = data.response[0].team;
-
-    const teamName = team.name;
-    const teamLogoUrl = team.logo;
-    const teamLogo = document.createElement('img');
-    teamLogo.src = teamLogoUrl;
-
-    const teamContainer = document.createElement('div');
-    teamContainer.classList.add('team-container');
-
-    const teamNameElement = document.createElement('h2');
-    teamNameElement.textContent = teamName;
-
-    teamContainer.appendChild(teamNameElement);
-    teamContainer.appendChild(teamLogo);
-    
-    // Add event listener to each team container
-    teamContainer.addEventListener('click', () => {
-      // Store the team data in local storage
-      localStorage.setItem('selectedTeam', JSON.stringify(team));
-      // Redirect to the separate page
-      window.location.href = 'team.html';
-    });
-
-    document.getElementById('teams').appendChild(teamContainer);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-}
-
-fetch("https://v3.football.api-sports.io/players?id=276&season=2019", {
+fetch("https://v3.football.api-sports.io/players?id=278&season=2019", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "v3.football.api-sports.io",
@@ -62,7 +18,7 @@ fetch("https://v3.football.api-sports.io/players?id=276&season=2019", {
 // Retrieve the player details from the API and navigate to the details page
 function fetchPlayerDetails() {
   // Make an API request to fetch the player details
-  fetch("https://v3.football.api-sports.io/players?id=276&season=2019", {
+  fetch("https://v3.football.api-sports.io/players?id=278&season=2019", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "v3.football.api-sports.io",
@@ -81,7 +37,7 @@ function fetchPlayerDetails() {
       const nationality = player.nationality;
       const photo = player.photo;
       const weight = player.weight;
-      const logo = player.statistics[0].team.logo;
+      const logo = data.response[0].statistics[0].team.logo;
 
       // Save the player details in sessionStorage to access them on the details page
       sessionStorage.setItem('playerDetails', JSON.stringify({
@@ -105,4 +61,4 @@ function fetchPlayerDetails() {
 }
 
 // Attach the click event listener to the button
-document.getElementById('player-details-button').addEventListener('click', fetchPlayerDetails);
+document.getElementById('player-photo').addEventListener('click', fetchPlayerDetails);
