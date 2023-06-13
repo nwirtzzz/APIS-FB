8fa5ca3456856d5b0628771f361358569f6a4215
Francisco Ariel López <112632960+falopez221@users.noreply.github.com>
2023-06-13 15:26:06 -0300

    Update details.js

--- details.js
+++ details.js
@@ -1,7 +1,7 @@
-// Retrieve the player details from sessionStorage
+
 const playerDetails = JSON.parse(sessionStorage.getItem('playerDetails'));
 
-// Display the player details on the page
+
 document.getElementById('player-name').textContent = playerDetails.firstName + ' ' + playerDetails.lastName;
 document.getElementById('player-age').textContent = playerDetails.age;
 document.getElementById('player-height').textContent = playerDetails.height;
